8840f245efa54f1eb60f07dc5e475634a3971902
Student <student@Makerss-MacBook-Pro.local>
2019-07-02 16:13:56 +0100

    add entry station to touch in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,11 +5,12 @@ class Oystercard
    DEFAULT_MAX_BALANCE = 90
    MINIMUM_FARE = 1
   attr_accessor :balance, :max_balance, :money
+  attr_reader :entry_station
 
   def initialize(balance = 0, max_balance = DEFAULT_MAX_BALANCE)
     @balance = balance
     @max_balance = max_balance
-    @in_journey = false
+    @entry_station = nil
     # @money = money
   end
 
@@ -18,18 +19,18 @@ class Oystercard
     @balance += money
   end
 
-  def touch_in
+  def touch_in(entry_station)
     raise 'Insufficient funds - please top up' if @balance < MINIMUM_FARE # balance also works here
-    @in_journey = true
+    @entry_station = entry_station
   end
 
   def touch_out
     deduct(MINIMUM_FARE)
-    @in_journey = false
+    @entry_station = nil
   end
 
   def in_journey?
-    @in_journey
+    @entry_station != nil
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,7 @@ require 'faker'
 describe Oystercard do
   subject(:oystercard) { described_class.new }
   let(:money) { Faker::Number.between(1, 10) }
+  let(:station) { double(station) }
   describe '#new' do
     it "has no balance when inititialized" do
       expect(oystercard.balance).to eq 0
@@ -25,24 +26,29 @@ describe Oystercard do
     describe 'touch_in' do
       it 'is in journey if card is touched in' do
         oystercard.top_up(10)
-        oystercard.touch_in
+        oystercard.touch_in(:station)
         expect(oystercard.in_journey?).to eq true
       end
       it 'raises error if balance is below touch in minimum' do
-        expect { oystercard.touch_in }.to raise_error 'Insufficient funds - please top up'
+        expect { oystercard.touch_in(:station) }.to raise_error 'Insufficient funds - please top up'
+      end
+      it 'records entry station after the touch in' do
+        oystercard.top_up(10)
+        oystercard.touch_in(:station)
+        expect(oystercard.entry_station).to eq :station
       end
     end
 
     describe 'touch_out' do
       it 'is not in journey if card is touched out' do
         oystercard.top_up(10)
-        oystercard.touch_in
+        oystercard.touch_in(:station)
         oystercard.touch_out
         expect(oystercard.in_journey?).to eq false
       end
       it 'charges minimum fare when touch out' do
         oystercard.top_up(10)
-        oystercard.touch_in
+        oystercard.touch_in(:station)
         expect { oystercard.touch_out }.to change{oystercard.balance}.by(-Oystercard::MINIMUM_FARE)
       end
     end
