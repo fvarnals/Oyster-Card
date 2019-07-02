3decc575cfa4aa4042b934af6f4fce5cd5430160
f.varnals@gmail.com <student@Makerss-MacBook-Pro.local>
2019-07-02 15:09:23 +0100

    Deduct money to charge for a journey upon touch out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 class Oystercard
    DEFAULT_MAX_BALANCE = 90
-
+   MINIMUM_FARE = 1
   attr_accessor :balance, :max_balance, :money
 
   def initialize(balance = 0, max_balance = DEFAULT_MAX_BALANCE)
@@ -18,15 +18,13 @@ class Oystercard
     @balance += money
   end
 
-  def deduct(fare)
-    @balance -= fare
-  end
-
   def touch_in
+    raise 'Insufficient funds - please top up' if @balance < MINIMUM_FARE # balance also works here
     @in_journey = true
   end
 
   def touch_out
+    deduct(MINIMUM_FARE)
     @in_journey = false
   end
 
@@ -39,6 +37,10 @@ class Oystercard
   def max(money)
     money + @balance > DEFAULT_MAX_BALANCE #also works with balance
   end
+end
+
+def deduct(fare)
+  @balance -= fare
 end
 
 binding.pry
---
+++ spec/oystercard_spec.rb
@@ -0,0 +1,51 @@
+require 'oystercard'
+require 'faker'
+
+describe Oystercard do
+  subject(:oystercard) { described_class.new }
+  let(:money) { Faker::Number.between(1, 10) }
+  describe '#new' do
+    it "has no balance when inititialized" do
+      expect(oystercard.balance).to eq 0
+    end
+    it 'has a set max of £90' do
+    expect(oystercard.max_balance).to eq Oystercard::DEFAULT_MAX_BALANCE
+    end
+    it 'raises an error if someone tries to put more than £90' do
+      oystercard.top_up(Oystercard::DEFAULT_MAX_BALANCE)
+      expect { oystercard.top_up(money) }.to raise_error 'Max top up allowed is £90. Please select different amount'
+    end
+
+  end
+  describe '#top_up' do
+    it 'we want a top-up method which allows adding money to balance' do
+      expect(oystercard.top_up(money)).to eq (0 + money)
+    end
+
+    describe 'touch_in' do
+      it 'is in journey if card is touched in' do
+        oystercard.top_up(10)
+        oystercard.touch_in
+        expect(oystercard.in_journey?).to eq true
+      end
+      it 'raises error if balance is below touch in minimum' do
+        expect { oystercard.touch_in }.to raise_error 'Insufficient funds - please top up'
+      end
+    end
+
+    describe 'touch_out' do
+      it 'is not in journey if card is touched out' do
+        oystercard.top_up(10)
+        oystercard.touch_in
+        oystercard.touch_out
+        expect(oystercard.in_journey?).to eq false
+      end
+      it 'charges minimum fare when touch out' do
+        oystercard.top_up(10)
+        oystercard.touch_in
+        expect { oystercard.touch_out }.to change{oystercard.balance}.by(-Oystercard::MINIMUM_FARE)
+      end
+    end
+
+  end
+  end
