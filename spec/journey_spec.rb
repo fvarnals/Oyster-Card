ea7e47b7bdd22f0e160fbf3a37472c103456a657
Freddie <f.varnals@gmail.com>
2019-07-03 14:50:19 +0100

    Going back to restart journey class creation

---
+++ spec/journey_spec.rb
@@ -0,0 +1,14 @@
+describe Journey do
+ it "receive a entry station on initialize" do
+
+ end
+ it "when oystercard taps out it should accept an exit station" do
+
+ end
+ it "should have a fare once journey complete" do
+
+ end
+ it "should set entry station to nil when tapping out without a tap in and fare to £6" do
+
+ end
+end
